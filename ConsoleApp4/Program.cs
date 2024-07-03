36844a17f5de8e5e6705b752529534d9f6869a11
MehemmedLinux <mehemedhumbetov1@example.com>
2024-07-03 20:16:02 +0400

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    class Dictionary
+    {
+        public Dictionary<string, string> translate = new Dictionary<string, string>();
+
+        public string this[string word]
+        {
+            get
+            {
+                try
+                {
+                    return translate[word];
+                }
+                catch (KeyNotFoundException)
+                {
+                    return "Word could not found";
+                }
+            }
+            set
+            {
+                translate[word] = value;
+            }
+        }
+    }
+    static void Main(string[] args)
+    {
+        Dictionary dictonary = new Dictionary();
+
+        dictonary["masin"] = "car";
+
+        Console.WriteLine($"masin : {dictonary["masin"]}");
+    }
+}
